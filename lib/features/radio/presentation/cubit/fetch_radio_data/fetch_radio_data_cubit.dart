b1215d3a722d9889968d23d048d0ef5f9c9547fb
Abduallh Elsaeid <ba390690@gmail.com>
2025-03-16 16:45:03 +0200

    refact fetch radio name and cubit

--- lib/features/radio/presentation/cubit/fetch_radio_data/fetch_radio_data_cubit.dart
+++ lib/features/radio/presentation/cubit/fetch_radio_data/fetch_radio_data_cubit.dart
@@ -3,22 +3,22 @@ import 'package:equatable/equatable.dart';
 import 'package:zeker_app/features/radio/data/models/radio_model/radio_model.dart';
 import 'package:zeker_app/features/radio/data/repositories/radio_repo.dart';
 
-part 'radio_state.dart';
+part 'fetch_radio_data_state.dart';
 
-class RadioCubit extends Cubit<RadioState> {
-  RadioCubit(this.radioRepo) : super(RadioInitial());
+class FetchRadioDataCubit extends Cubit<FetchRadioDataState> {
+  FetchRadioDataCubit(this.radioRepo) : super(FetchRadioDataInitial());
 
   final RadioRepo radioRepo;
 
   Future<void> fetchRadioData() async {
-    emit(RadioLoading());
+    emit(FetchRadioDataLoading());
     var result = await radioRepo.fetchRadioData();
     result.fold(
       (failure) {
-        emit(RadioFailure(errorMessage: failure.errorMessage));
+        emit(FetchRadioDataFailure(errorMessage: failure.errorMessage));
       },
       (radio) {
-        emit(RadioSuccess(radio: radio));
+        emit(FetchRadioDataSuccess(radio: radio));
       },
     );
   }
